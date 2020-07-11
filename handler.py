b45dc9bcc625ce57a0b90f337af7f28ceeca28d9
akashbhardwaj04 <akkiworkspace@gmail.com>
2020-07-11 19:34:22 +0530

    added xo-functionalities

--- handler.py
+++ handler.py
@@ -43,3 +43,32 @@ class XoTransactionHandler(TransactionHandler):
         raise InvalidTransaction("Invalid payload serialization")
 
     return signer, game_name, action, space
+
+    def _get_state_data(self, game_name, context):
+    game_address = self._make_game_address(game_name)
+
+    state_entries = context.get_state([game_address])
+
+    try:
+        return self._decode_data(state_entries[0].data)
+    except IndexError:
+        return None, None, None, None
+    except:
+        raise InternalError("Failed to deserialize game data.")
+
+    def _make_game_address(self, game_name):
+    prefix = self._namespace_prefix
+    game_name_utf8 = game_name.encode('utf-8')
+    return prefix + hashlib.sha512(game_name_utf8).hexdigest()[0:64]
+
+    def _store_game_data(self, game_name, game_data, context):
+    game_address = self._make_game_address(game_name)
+
+    encoded_game_data = self._encode_data(game_data)
+
+    addresses = context.set_state(
+        {game_address: encoded_game_data}
+    )
+
+    if len(addresses) < 1:
+        raise InternalError("State Error")
