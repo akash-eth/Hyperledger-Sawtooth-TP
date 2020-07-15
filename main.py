27dfa90aea1861dde5edc7655a28cd1a546e4c7e
akashbhardwaj04 <akkiworkspace@gmail.com>
2020-07-15 10:18:35 +0530

    updated dependencies

--- main.py
+++ main.py
@@ -12,4 +12,22 @@ def main():
     processor.add_handler(handler)
 
     processor.start()
-    
+
+from sawtooth_signing import create_context
+from sawtooth_signing import CryptoFactory
+
+context = create_context('secp256k1')
+private_key = context.new_random_private_key()
+signer = CryptoFactory(context).new_signer(private_key)
+
+import cbor
+
+payload = {
+    'Verb': 'set',
+    'Name': 'foo',
+    'Value': 42}
+
+payload_bytes = cbor.dumps(payload)
+
+from hashlib import sha512
+from sawtooth_sdk.protobuf.transaction_pb2 import TransactionHeader
