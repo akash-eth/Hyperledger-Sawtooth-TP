5bd2d07407e48f5862da454252586aff77b8a161
akashbhardwaj04 <akkiworkspace@gmail.com>
2020-07-19 17:40:09 +0530

    modified apply method

--- handler.py
+++ handler.py
@@ -1,25 +1,26 @@
 import sawtooth_sdk
-
-class XoTransactionHandler(TransactionHandler):
+class XoTransactionHandler:
     def __init__(self, namespace_prefix):
         self._namespace_prefix = namespace_prefix
 
     @property
     def family_name(self):
         return 'xo'
-    
+
     @property
-    def family_version(self):
+    def family_versions(self):
         return ['1.0']
-    
+
+    @property
+    def encodings(self):
+        return ['csv-utf8']
+
     @property
     def namespaces(self):
         return [self._namespace_prefix]
 
-    def apply(self, trasaction, context):
-
-        def apply(self, transaction, context):
-    signer, game_name, action, space = \
+    def apply(self, transaction, context):
+        signer, game_name, action, space = \
         self._unpack_transaction(transaction)
 
     board, state, player1, player2 = \
@@ -32,29 +33,3 @@ class XoTransactionHandler(TransactionHandler):
     )
 
     self._store_game_data(game_name, updated_game_data, context)
-
-    def _unpack_transaction(self, transaction):
-    header = transaction.header
-    signer = header.signer
-
-    try:
-        game_name, action, space = self._decode_data(transaction.payload)
-    except:
-        raise InvalidTransaction("Invalid payload serialization")
-
-    return signer, game_name, action, space
-
-    def _get_state_data(self, game_name, context):
-    game_address = self._make_game_address(game_name)
-
-    state_entries = context.get_state([game_address])
-
-    try:
-        return self._decode_data(state_entries[0].data)
-    except IndexError:
-        return None, None, None, None
-    except:
-        raise InternalError("Failed to deserialize game data.")
-
-
-   
